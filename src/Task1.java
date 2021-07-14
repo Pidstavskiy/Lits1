0c1e31d1e441927f3364231b7ef5fb97ad1884ac
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-07-14 16:53:35 +0300

    task1

--- src/Task1.java
+++ src/Task1.java
@@ -1,5 +1,21 @@
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        int a = 1;
+        int b = 8;
+        int e = 6;
+        int c = 4;
+        int i = 5;
+        int result = a + b / e + c * (i + a);
+        System.out.println("a + b / e + c * (i + a) = " + result);
+
+        System.out.println((char) 14);
+        System.out.println((char) 77);
+        System.out.println((char) 105);
+
+        System.out.println("Number 10 is even " + (10 % 2 == 0));
+        System.out.println("Number 13 is even " + (13 % 2 == 0));
+        System.out.println("Number 17 is even " + (17 % 2 == 0));
+        System.out.println("Number 20 is even " + (20 % 2 == 0));
     }
 }
