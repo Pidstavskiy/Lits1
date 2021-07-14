7527620a777e19786d7fb764115fac0ecdf27307
Pidstavskiy <vitalii.pidstavskiy@gmail.com>
2021-07-14 22:35:40 +0300

    update commit

--- src/Task1.java
+++ src/Task1.java
@@ -6,8 +6,17 @@ public class Task1 {
         int e = 6;
         int c = 4;
         int i = 5;
-        int result = a + b / e + c * (i + a);
-        System.out.println("a + b / e + c * (i + a) = " + result);
+        int z = 9;
+        int x = 4;
+        int p = 3;
+        int o = 5;
+        int l = 2;
+        int result1 = a + b / e + c * (i + a);
+        int result2 = z + x - e + p / a;
+        int result3 = o + i + l * (a + b);
+        System.out.println("a + b / e + c * (i + a) = " + result1);
+        System.out.println("z + x - e + p / a " + result2);
+        System.out.println("o + i + l * (a + b) = " + result3);
 
         System.out.println((char) 14);
         System.out.println((char) 77);
